7ead2fc96497b1aab4ac1bb0f9b1210786c93269
Andres Kaasik <andreskaasik@gmail.com>
2015-09-06 19:36:14 +0300

    Adding requests library and refactor video feed fetching

--- default.py
+++ default.py
@@ -6,6 +6,7 @@ import urllib
 import urlparse
 import xbmcgui
 import xbmcplugin
+from resources.lib import requests
 from resources.lib import feedparser
 
 base_url = sys.argv[0]
@@ -15,34 +16,25 @@ url_etv = 'rtsp://194.36.162.51:80/live/etv'
 url_etv_logo = 'http://etv2.err.ee/Content/images/global/etv-logo.png'
 url_etv2 = 'rtsp://194.36.162.51:80/live/etv2'
 url_etv2_logo = 'http://etv2.err.ee/Content/images/global/etv2-logo.png'
-url_err_rss = 'http://uudised.err.ee/rss'
+url_rss = 'http://uudised.err.ee/rss'
 
 def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
-def read_post(post, queue):
-    data = urllib.urlopen(post.link).read()
-    media_count = string.count(data, '://media.err.ee:80/')
-    if media_count == 1:
-        print(post.title.encode('utf8'))
-        print '  ' + post.link
-        pos1 = string.find(data, '://media.err.ee:80/')
-        pos2 = string.find(data, '@', pos1)
-        pos3 = string.find(data, '.mp4', pos2)
-        url_media = 'rtsp' + data[pos1:pos3]
-        post.video = url_media + '/mp4:' + data[pos2 + 1:pos3]
-        print post.video
-        queue.put(post)
+def read_post(p,q):
+    session = requests.Session()
+    p.html = session.get(p.link).text
+    q.put(p)
 
-def fetch_videos():
-    feed = feedparser.parse(url_err_rss)
-    result = Queue.Queue()
-    threads = [threading.Thread(target=read_post, args = (post,result)) for post in feed.entries]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
-    return result
+def parse_video_url(html):
+    video_url = ''
+    pos1 = string.find(html, '://media.err.ee:80/')
+    if pos1 > -1:
+        pos2 = string.find(html, '@', pos1)
+        pos3 = string.find(html, '.mp4', pos2)
+        if pos2 > -1 and pos3 > 1:
+            video_url = 'rtsp' + html[pos1:pos3] + '/mp4:' + html[pos2 + 1:pos3]
+    return video_url
 
 mode = args.get('mode', None)
 	
@@ -53,17 +45,27 @@ if mode is None:
     url = build_url({'mode': 'videos'})
     li = xbmcgui.ListItem('Videos', iconImage='DefaultFolder.png')
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
 elif mode[0] == 'live':
     li = xbmcgui.ListItem('ETV', iconImage=url_etv_logo)
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url_etv, listitem=li)
     li = xbmcgui.ListItem('ETV2', iconImage=url_etv2_logo)
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url_etv2, listitem=li)
+
 elif mode[0] == 'videos':
-    q = fetch_videos()
+    feed = feedparser.parse(url_rss)
+    q = Queue.Queue()
+    threads = [threading.Thread(target=read_post, args = (p,q)) for p in feed.entries]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
     while not q.empty():
-        post = q.get()
-        li = xbmcgui.ListItem(post.title, iconImage='DefaultVideo.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=post.video, listitem=li)
+        p = q.get()
+        video_url = parse_video_url(p.html)
+        if video_url != '':
+            li = xbmcgui.ListItem(p.title, iconImage='DefaultVideo.png')
+            xbmcplugin.addDirectoryItem(handle=addon_handle, url=video_url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
 xbmcplugin.setContent(addon_handle, 'movies')
